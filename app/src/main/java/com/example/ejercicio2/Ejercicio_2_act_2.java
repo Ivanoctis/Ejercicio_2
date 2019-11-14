5f7d9d24064e01d383bb6cedd03804d6e169f3cd
Ivanoctis <57736947+Ivanoctis@users.noreply.github.com>
2019-11-13 20:43:27 -0600

    Initial commit

---
+++ app/src/main/java/com/example/ejercicio2/Ejercicio_2_act_2.java
@@ -0,0 +1,14 @@
+package com.example.ejercicio2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Ejercicio_2_act_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ejercicio_2_act_2);
+    }
+}
